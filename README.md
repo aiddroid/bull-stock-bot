da56e57db01731c3df9b6aebec18e1e09743c056
Allen <aiddroid@gmail.com>
2021-01-16 22:08:37 +0800

    feat:update README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # bull-stock-bot
 Bull stock bot for telegram.
+
+### setup
+export these environment variables to your operating system.
+```
+export BOT_TOKEN="You can create a telegram bot and get bot token from https://t.me/botfather"
+export FINNHUB_TOKEN="You can get finnhub stock api token from https://finnhub.io"
+```
+
+install python requirements
+```
+pip3 install -r requirements.txt
+```
+
+### start the bot
+```
+python3 main.py
+```
--- config.py
+++ config.py
@@ -8,7 +8,10 @@
 Bot config.
 
 """
+import os
 
-BOT_TOKEN="You can create a telegram bot and get bot token from https://t.me/botfather"
-FINNHUB_TOKEN="You can get finnhub stock api token from https://finnhub.io"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "You can create a telegram bot and get bot token from https://t.me/botfather")
+FINNHUB_TOKEN = os.getenv("FINNHUB_TOKEN", "You can get finnhub stock api token from https://finnhub.io")
 
+print("BOT_TOKEN: " + BOT_TOKEN)
+print("FINNHUB_TOKEN: " + FINNHUB_TOKEN)
